9330afd9b2eb158eefdfc17f96a121d72288ea0b
Minh Nguyen <minhnh.91@gmail.com>
2019-05-04 09:47:51 +0200

    fix erros

--- ros/src/topological_map_ros/topological_map.py
+++ ros/src/topological_map_ros/topological_map.py
@@ -10,29 +10,45 @@ from geometry_msgs.msg import PoseWithCovarianceStamped
 from topological_map_ros.srv import TopologicalPath, TopologicalPathResponse, TopologicalPosition, TopologicalPositionResponse
 
 
-from topological_map_ros.script import generate_map
+def generate_map():
+    G = nx.DiGraph()
+
+    G.add_edge('hallway', 'entry', through='entrance door shown in the map', level='room')
+    G.add_edge('hallway', 'bar', through='entry', level='room')
+    G.add_edge('hallway', 'bedroom', through='door', level='room')
+    G.add_edge('hallway', 'living room', through='corridor oposite the entrance door', level='room')
+    G.add_edge('bedroom', 'kitchen', through='door next to the desk', level='room')
+    G.add_edge('kitchen', 'living room', through='corridor between the white drawer and the blue trash can', level='room')
+    G.add_edge('living room', 'exit', through='door next to the coat hanger', level='room')
+
+    G.add_edge('entry', 'hallway', through='entrance door', level='room')
+    G.add_edge( 'bar', 'hallway', through='corridor oposite the bar table', level='room')
+    G.add_edge('bedroom', 'hallway',  through='door oposite the desk', level='room')
+    G.add_edge('living room', 'hallway',  through='corridor between the tv and the bookcase', level='room')
+    G.add_edge('kitchen', 'bedroom',  through='door between the cupboard and the blue trash can', level='room')
+    G.add_edge('living room', 'kitchen',  through='corridor between the bookcase and the high table', level='room')
+    G.add_edge('exit', 'living room',  through='exit door shown in the map', level='room')
+
+    # return G.to_directed()
+    return G
+
 
 class TopologicalMap(object):
     def __init__(self):
 
-        self.server = rospy.Service('topological_path_plan', TopologicalPath, handle_path_request)
-        self.position_server = rospy.Service('topological_position', TopologicalPosition, handle_position_request)
+        self.server = rospy.Service('topological_path_plan', TopologicalPath, self.handle_path_request)
+        self.position_server = rospy.Service('topological_position', TopologicalPosition, self.handle_position_request)
 
-        self.rooms_config = rospy.get_param('rooms')
-        print(self.rooms_config)
+        self.rooms_config = rospy.get_param('~rooms', dict())
 
         self.rooms = dict()
         # TODO get room poses from param
         for room, points in self.rooms_config.items():
-            self.rooms[room] = add_room(points)
+            self.rooms[room] = self.add_room(points)
 
         self.G = generate_map()
 
-
-        rospy.init_node('topological_map_server')
         rospy.loginfo("Initialized topological map server")
-
-
 
     def add_room(self, point_list):
         # TODO use the point list loaded from the config, each point in the list needs to be changed to a tuple like below
@@ -59,10 +75,8 @@ class TopologicalMap(object):
         y = -3.7
 
         robot_pose = Point(x, y) # Inspection test pose
-        # for room in self.rooms:
-        room.encloses_point(robot_pose)
-
-
+        #for room in self.rooms:
+        self.rooms['living_room'].encloses_point(robot_pose)
 
         position = 'hallway'
         rospy.loginfo("Robot is in %s" % position )
